198a28906941ae8b2903e951e67a63d20a7f6777
JILopez1015 <lopezjasmin1015@gmail.com>
2022-04-15 12:06:27 -0400

    Recent Work 4/15

--- Final_Research_Project_ JL.R
+++ Final_Research_Project_ JL.R
@@ -1,34 +1,137 @@
 
 #####    Final Project #####
-#####    4/1/2021      #####
+#####    4/15/2021      #####
 
-####     loading packages ####
+#####   Loading packages                    ####
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
+library(lubridate)
 
 
-#####    Loading in Temp data #####
+#####   Loading in Temp data                #####
 
 ##metadata link https://ag.arizona.edu/azmet/raw2003.htm
 
 T17 <- read.csv(url("https://ag.arizona.edu/azmet/data/0617rh.txt"), 
-                   header = FALSE)
+                header = FALSE)
+colnames(T17) <- c("Year", "Day of Year (DOY)","Hour of Day", "Air Temperature",
+                   "Rel. Humidity","Vapor Pressure Deficit","Solar Radiation",
+                   "Precipitation","Soil Temperature  ( = 2in prior to 1999 )",
+                   "20in Soil Temperature  ( = 4in prior to 1999 )", 
+                   "Wind Speed (Ave)","Wind Vector Magnitude","Wind Vector Direction",
+                   " Wind Direction Standard Deviation","Max Wind Speed",
+                   "Reference Evapotranspiration (ETo)")
 
 T18 <- read.csv(url("https://ag.arizona.edu/azmet/data/0618rh.txt"),
-                    header=FALSE)
+                header=FALSE)
+colnames(T18) <- c("Year", "Day of Year (DOY)","Hour of Day", "Air Temperature",
+                   "Rel. Humidity","Vapor Pressure Deficit","Solar Radiation",
+                   "Precipitation","Soil Temperature  ( = 2in prior to 1999 )",
+                   "20in Soil Temperature  ( = 4in prior to 1999 )", 
+                   "Wind Speed (Ave)","Wind Vector Magnitude","Wind Vector Direction",
+                   " Wind Direction Standard Deviation","Max Wind Speed",
+                   "Reference Evapotranspiration (ETo)")
 
 T19 <- read.csv(url("https://ag.arizona.edu/azmet/data/0619rh.txt"),
                 header=FALSE)
+colnames(T19) <- c("Year", "Day of Year (DOY)","Hour of Day", "Air Temperature",
+                   "Rel. Humidity","Vapor Pressure Deficit","Solar Radiation",
+                   "Precipitation","Soil Temperature  ( = 2in prior to 1999 )",
+                   "20in Soil Temperature  ( = 4in prior to 1999 )", 
+                   "Wind Speed (Ave)","Wind Vector Magnitude","Wind Vector Direction",
+                   " Wind Direction Standard Deviation","Max Wind Speed",
+                   "Reference Evapotranspiration (ETo)")
 
 T20 <- read.csv(url("https://ag.arizona.edu/azmet/data/0620rh.txt"),
                 header=FALSE)
+colnames(T20) <- c("Year", "Day of Year (DOY)","Hour of Day", "Air Temperature",
+                   "Rel. Humidity","Vapor Pressure Deficit","Solar Radiation",
+                   "Precipitation","Soil Temperature  ( = 2in prior to 1999 )",
+                   "20in Soil Temperature  ( = 4in prior to 1999 )", 
+                   "Wind Speed (Ave)","Wind Vector Magnitude","Wind Vector Direction",
+                   " Wind Direction Standard Deviation","Max Wind Speed",
+                   "Reference Evapotranspiration (ETo)")
 
 T21 <- read.csv(url("https://azmet.arizona.edu/azmet/data/0621rh.txt"),
                 header=FALSE)
+colnames(T21) <- c("Year", "Day of Year (DOY)","Hour of Day", "Air Temperature",
+                   "Rel. Humidity","Vapor Pressure Deficit","Solar Radiation",
+                   "Precipitation","Soil Temperature  ( = 2in prior to 1999 )",
+                   "20in Soil Temperature  ( = 4in prior to 1999 )", 
+                   "Wind Speed (Ave)","Wind Vector Magnitude","Wind Vector Direction",
+                   " Wind Direction Standard Deviation","Max Wind Speed",
+                   "Reference Evapotranspiration (ETo)")
+#####   Loading in Electricity Demand Data  ####
 
-#####   Formatting Raw datT   #####
- 
-datT<-dataT %>% seperate(X2017.1.6.13.6.9.3.11.8.92.7.69.2.83.5..23.7.52.1.78.14.5.12.5.13.6.14.8.14.7.14.8.3.5.2.6.188.43.10.9..2.1.1.2.1.16.9)
+E17 <- read.csv("Z:\\students/jlopez/Research Project/Data/EIA_Demand_5yr/EIA_1.17_12.17.csv", 
+                header= TRUE, )
+
+E18 <- read.csv("Z:\\students/jlopez/Research Project/Data/EIA_Demand_5yr/EIA_1.18_12.18.csv", 
+                   header= TRUE)
+
+E19 <- read.csv("Z:\\students/jlopez/Research Project/Data/EIA_Demand_5yr/EIA_1.19_12.19.csv", 
+                    header= TRUE)
+
+E20 <- read.csv("Z:\\students/jlopez/Research Project/Data/EIA_Demand_5yr/EIA_1.20_12.20.csv", 
+            header= TRUE)
+
+E21 <- read.csv("Z:\\students/jlopez/Research Project/Data/EIA_Demand_5yr/EIA_1.21_12.21.csv", 
+                header= TRUE)
+
+#####   Formatting Time on ED Data          ####
+
+#convert date and time
+datesD <- as.Date(E17$Timestamp..Hour.Ending., "%m/%d/%Y")
+datesD.2 <- as.Date(E18$Timestamp..Hour.Ending., "%m/%d/%Y")
+datesD.3 <- as.Date(E19$Timestamp..Hour.Ending., "%m/%d/%Y")
+datesD.4 <- as.Date(E20$Timestamp..Hour.Ending., "%m/%d/%Y")
+datesD.5 <- as.Date(E21$Timestamp..Hour.Ending., "%m/%d/%Y")
+#get day of year
+E17$doy <- yday(datesD)
+E18$doy <- yday(datesD.2)
+E19$doy <- yday(datesD.3)
+E20$doy <- yday(datesD.4)
+E21$doy <- yday(datesD.5)
+
+#calculate year
+E17$year <- year(datesD)
+E18$year <- year(datesD.2)
+E19$year <- year(datesD.3)
+E20$year <- year(datesD.4)
+E21$year <- year(datesD.5)
+
+#defining month
+E17$month<-month(datesD)
+E18$month<-month(datesD.2)
+E19$month<-month(datesD.3)
+E20$month<-month(datesD.4)
+E21$month<-month(datesD.5)
 
 
+#####   Histograms Temp                     #####
+
+H17 <- hist(T17[,4]  , main= "'17 Temp Distribution", xlab= "Temp")
+
+H18 <- hist(T18[,4]  , main= "18 Temp Distribution", xlab= "Temp")
+
+H19 <- hist(T19[,4]  , main= "'19 Temp Distribution", xlab= "Temp")
+
+H20 <- hist(T20[,4] , main= "'20 Temp Distribution", xlab= "Temp")
+
+H21 <- hist(T21[,4]  , main= "'21 Temp Distribution", xlab= "Temp")
+
+
+#####   Histogram Electricity Demand        ####
+EH17 <- hist(E17$Demand..MWh. , main= "'17 Electricity Demand", xlab= "Elec")
+
+EH18 <- hist(E18$Demand..MWh. , main= "18 Electricity Demand", xlab= "Elec")
+
+EH19 <- hist(E19$Demand..MWh. , main= "'19 Electricity Demand", xlab= "Elec")
+
+EH20 <- hist(E20$Demand..MWh. , main= "'20 Electricity Demand", xlab= "Elec")
+
+EH21 <- hist(E21$Demand..MWh. , main= "'21 Electricity Demand", xlab= "Elec")
+
+#####   Plotting                 ####
+TE17 <- 

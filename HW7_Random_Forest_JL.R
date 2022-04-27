a5fc27743d45ceaa01f3862c0bbc7c9232f5e98c
JILopez1015 <lopezjasmin1015@gmail.com>
2022-04-27 12:11:43 -0400

    HW 7 beginning of code

---
+++ HW7_Random_Forest_JL.R
@@ -0,0 +1,45 @@
+
+#####    HW7: Random Forest Classification
+#####    4/27/2021 
+
+####   Notes ####
+#evaluating itself because it is iterative, not using all of its data
+#cross validation: how many ensembles do we want to use (10 is a goof rule of thumb)
+  #10 model runs, 10 trees to one model
+#Q3 maybe find it on internet
+#Q4 interpreting results
+
+####    Code         ####
+####    Packages     ####
+install.packages(c("caret","randomForest")) 
+library(terra)
+library(caret)
+library(randomForest)
+
+#set up working directory for oneida data folder on the server
+setwd("Z:/data/oneida/")
+
+####  Sentinel data  ####
+
+# list files from sentinel satellite image files
+f <- list.files(path = "sentinel/", pattern = "T18", full.names = T)
+
+# read the list of files as a single multi-band spatRaster
+rsdat <- rast(f)
+
+# create a vector of band names so we can keep track of them
+b <- c("B2","B3","B4","B5","B6","B7","B8","B11","B12")
+
+#set band names in our raster
+names(rsdat) <- b
+
+#### Cloud mask data file    ####
+clouds <- rast("sentinel/MSK_CLDPRB_20m.tif")
+
+#### Validation data ####
+algae <- vect("Oneida/algae.shp")
+agri <- vect("Oneida/agriculture.shp")
+built <- vect("Oneida/built.shp")
+forest <- vect("Oneida/forest.shp")
+water <- vect("Oneida/water.shp")
+wetlands <- vect("Oneida/wetlands.shp")
